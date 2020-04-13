f14706aea659ee0166a2abb5653f33fd88cb5f88
J <email@example.com>
2020-04-13 13:08:59 +0000

    List of rides that user is a part of

--- app/src/main/java/is/hi/hopon/backend/HoponBackend.java
+++ app/src/main/java/is/hi/hopon/backend/HoponBackend.java
@@ -2,18 +2,28 @@ package is.hi.hopon.backend;
 
 
 
+import android.graphics.Point;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import is.hi.hopon.HoponContext;
@@ -21,6 +31,7 @@ import is.hi.hopon.backend.Models.Core.Model;
 import is.hi.hopon.backend.Models.Login.LoginRequest;
 import is.hi.hopon.backend.Models.Login.LoginResponse;
 import is.hi.hopon.backend.Models.Login.UserDetails;
+import is.hi.hopon.backend.Models.Ride;
 
 public class HoponBackend {
     private String url;
@@ -103,5 +114,83 @@ public class HoponBackend {
         hoponBackendRequest("users/"+id, Request.Method.GET, null, new UserDetails(), resultListener, true);
     }
 
+    private void jsonArrayRequest(String endpoint, int method, JSONArray payload, HoponBackendResponse<JSONArray> results)
+    {
+        if(HoponContext.getInstance().getUser() == null)
+            {
+                results.onError(new AuthenticationRequired("This endpoint requires authentication"));
+            }
+            JsonArrayRequest request = new JsonArrayRequest(
+                method, url + endpoint, payload,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        results.onSuccess(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                if(error instanceof AuthFailureError){
+                    HoponContext.getInstance().logOut();
+                }
+                results.onError(error);
+            }
+        }
+        ){
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                String bearer = "Bearer " + HoponContext.getInstance().getUser().getToken();
+                headers.put("Authorization", bearer);
+                Log.i("HLOG/hoponHeaders", bearer);
+                return headers;
+            }
+        };
+
+        HoponContext.getInstance().getWebQueue().enqueue(request);
+    }
+
+    public void rides(HoponBackendResponse<List<Ride>> result)
+    {
+        jsonArrayRequest("rides", Request.Method.GET, null, new HoponBackendResponse<JSONArray>() {
+            @Override
+            public void onSuccess(JSONArray response) {try {
+                Integer myId = Integer.parseInt(HoponContext.getInstance().getUser().getId());
+                ArrayList<Ride> rideList = new ArrayList<Ride>();
+                for(int i=0; i<response.length();i++){
+                    JSONObject entry = response.getJSONObject(i);
+                    JSONArray jsonPassengers = entry.getJSONArray("passengers");
+                    ArrayList<Integer> passengers = new ArrayList<Integer>();
+                    for(int p=0; p<jsonPassengers.length();p++){
+                        passengers.add(jsonPassengers.getInt(p));
+                    }
+                    JSONArray jsonOrigin = entry.getJSONObject("origin").getJSONArray("coordinates");
+                    JSONArray jsonDest = entry.getJSONObject("destination").getJSONArray("coordinates");
+                    Integer driver = entry.getInt("driver");
+
+                    if(!(entry.getInt("driver") == myId || passengers.contains(myId))) continue;
+
+                    rideList.add(new Ride(
+                       entry.getInt("driver"),
+                       entry.getString("departureTime"),
+                       entry.getInt("duration"),
+                       new Point(jsonOrigin.getInt(0), jsonOrigin.getInt(1)),
+                       new Point(jsonDest.getInt(0), jsonDest.getInt(1)),
+                       entry.getInt("freeSeats"),
+                       passengers
+                    ));
+                }
+                result.onSuccess(rideList);
+            } catch (JSONException e) {
+                result.onError(e);
+            }
+            }
+
+            @Override
+            public void onError(Exception error) {
+                result.onError(error);
+            }
+        });
+    }
 
 }
